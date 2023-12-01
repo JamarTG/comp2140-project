a40356df8310e13f021dc2aa1f8e9286aa20b4cf
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2023-12-01 09:53:03 -0500

    minor bug fixes before presentation

--- server/app.js
+++ server/app.js
@@ -1,215 +1,278 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const db = require("./db");
 
-// models
 const User = require("./model/user");
 const Book = require("./model/book");
 const Rental = require("./model/rental");
+
 const app = express();
 const port = 3000;
 
-
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "../client")));
+app.use(express.static(path.join(__dirname, "../view")));
 
 app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  const existingUserCount = await User.countDocuments();
+    const {
+        username,
+        password
+    } = req.body;
+    const existingUserCount = await User.countDocuments();
 
-  if (existingUserCount > 0) {
-    return res
-      .status(400)
-      .json({ message: "User already exists. Registration not allowed." });
-  }
-
-  try {
-    const existingUser = await User.findOne({ username });
-
-    if (existingUser) {
-      return res.status(400).json({ message: "Username already exists" });
+    if (existingUserCount > 0) {
+        return res
+            .status(400)
+            .json({
+                message: "User already exists. Registration not allowed."
+            });
     }
 
-    const newUser = new User({ username, password });
-    await newUser.save();
+    try {
+        const existingUser = await User.findOne({
+            username
+        });
 
-    res.status(201).json({ message: "User registered successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to register user" });
-  }
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists"
+            });
+        }
+
+        const newUser = new User({
+            username,
+            password
+        });
+        await newUser.save();
+
+        res.status(201).json({
+            message: "User registered successfully"
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Failed to register user"
+        });
+    }
 });
 
 app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
+    const {
+        username,
+        password
+    } = req.body;
 
-  try {
-    // Find the user by username
-    const user = await User.findOne({ username });
+    try {
+        const user = await User.findOne({
+            username
+        });
 
-    if (!user) {
-      return res.status(401).json({ message: "Invalid username or password" });
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid username or password"
+            });
+        }
+
+        const passwordMatch = await user.comparePassword(password);
+
+        if (passwordMatch) {
+            return res.status(200).json({
+                message: "Login successful"
+            });
+        } else {
+            return res.status(401).json({
+                message: "Invalid username or password"
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            error: "Login failed"
+        });
     }
-
-    // Compare the provided password with the stored hashed password
-    const passwordMatch = await user.comparePassword(password);
-
-    if (passwordMatch) {
-      
-      return res.status(200).json({ message: "Login successful" });
-    } else {
-      return res.status(401).json({ message: "Invalid username or password" });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Login failed" });
-  }
 });
 
 app.get("/books/:gradeLevel", async (req, res) => {
-  const { gradeLevel } = req.params;
+    const {
+        gradeLevel
+    } = req.params;
 
-  try {
-    const books = await Book.find({ gradeLevel: parseInt(gradeLevel) });
+    try {
+        const books = await Book.find({
+            gradeLevel: parseInt(gradeLevel)
+        });
 
-    if (!books || books.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "No books found for this grade level" });
+        res.json({
+            message: `Books for grade level ${gradeLevel} retrieved successfully`,
+            data: books,
+        });
+    } catch (error) {
+        console.error(error);
+        res
+            .status(500)
+            .json({
+                error: "Failed to retrieve books for this grade level"
+            });
     }
-
-    res.json({
-      message: `Books for grade level ${gradeLevel} retrieved successfully`,
-      data: books,
-    });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ error: "Failed to retrieve books for the grade level" });
-  }
 });
 
 app.post("/books", async (req, res) => {
-  const { title, price, gradeLevel, authors, publisher, ISBN } = req.body;
-
-  try {
-    const existingBook = await Book.findOne({ ISBN });
-
-    if (existingBook) {
-      existingBook.title = title;
-      existingBook.price = price;
-      existingBook.gradeLevel = gradeLevel;
-      existingBook.authors = authors;
-      existingBook.publisher = publisher;
-
-      const updatedBook = await existingBook.save();
-      res.json({ message: "Book updated successfully", data: updatedBook });
-    } else {
-      const newBook = new Book({
+    const {
         title,
         price,
         gradeLevel,
         authors,
         publisher,
-        ISBN,
-      });
-      const savedBook = await newBook.save();
-      res.json({ message: "Book added successfully", data: savedBook });
+        ISBN
+    } = req.body;
+
+    try {
+        const existingBook = await Book.findOne({
+            ISBN
+        });
+
+        if (existingBook) {
+            existingBook.title = title;
+            existingBook.price = price;
+            existingBook.gradeLevel = gradeLevel;
+            existingBook.authors = authors;
+            existingBook.publisher = publisher;
+
+            const updatedBook = await existingBook.save();
+            res.json({
+                message: "Book updated successfully",
+                data: updatedBook
+            });
+        } else {
+            const newBook = new Book({
+                title,
+                price,
+                gradeLevel,
+                authors,
+                publisher,
+                ISBN,
+            });
+            const savedBook = await newBook.save();
+            res.json({
+                message: "Book added successfully",
+                data: savedBook
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res
+            .status(500)
+            .json({
+                error: "Failed to add/update book in the database"
+            });
     }
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ error: "Failed to add/update book in the database" });
-  }
 });
 
 app.post("/rental", async (req, res) => {
-  const { studentName, schoolID, contact, bookIDs, dueDate } = req.body;
-
-  try {
-    // Check if there's an existing rental for the same student ID
-    const existingRental = await Rental.findOne({ schoolID });
-
-    if (existingRental) {
-      // If a rental for the same student ID exists, update its details
-      existingRental.studentName = studentName;
-      existingRental.contact = contact;
-      existingRental.dueDate = dueDate;
-
-      // Add the new bookIDs to the existing rental's books array
-
-      const updatedRental = await existingRental.save();
-      res.json({
-        message: "Rental Details updated successfully",
-        data: updatedRental,
-      });
-    } else {
-      // If no rental for the same student ID exists, create a new rental
-      const rentalDetail = new Rental({
+    const {
         studentName,
         schoolID,
         contact,
-        dueDate,
-      });
+        gradeLevel,
+        dueDate
+    } = req.body;
 
-      const savedRentalDetail = await rentalDetail.save();
-      res.json({
-        message: "Rental Details added successfully",
-        data: savedRentalDetail,
-      });
+    try {
+        const existingRental = await Rental.findOne({
+            schoolID
+        });
+        if (existingRental) {
+            existingRental.studentName = studentName;
+            existingRental.contact = contact;
+            existingRental.dueDate = dueDate;
+
+            const updatedRental = await existingRental.save();
+            res.json({
+                message: "Rental Details updated successfully",
+                data: updatedRental,
+            });
+        } else {
+            const rentalDetail = new Rental({
+                studentName,
+                schoolID,
+                contact,
+                dueDate,
+                gradeLevel
+            });
+
+            const savedRentalDetail = await rentalDetail.save();
+            res.json({
+                message: "Rental Details added successfully",
+                data: savedRentalDetail,
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res
+            .status(500)
+            .json({
+                error: "Failed to save rental details to the database"
+            });
     }
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ error: "Failed to save rental details to the database" });
-  }
 });
 
 app.get("/rental-details", async (req, res) => {
-  try {
-    const allRentalDetails = await Rental.find();
-    res.json({ data: allRentalDetails });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to get all rental details" });
-  }
+    try {
+        const allRentalDetails = await Rental.find();
+        res.json({
+            data: allRentalDetails
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: "Failed to get all rental details"
+        });
+    }
 });
 
-app.get("/overdue-books", async (req, res) => {
-  try {
-    const today = new Date();
-    const overdueBooks = await Book.find({ dueDate: { $lt: today } });
-    res.json({ data: overdueBooks });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to get overdue books" });
-  }
+app.get("/overdue-rentals", async (req, res) => {
+    try {
+        const today = new Date();
+        const overdueBooks = await Rental.find({
+            dueDate: {
+                $lt: today
+            }
+        });
+        res.json({
+            data: overdueBooks
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: "Failed to get overdue books"
+        });
+    }
 });
 
 app.put("/book-returns/:rentalID", async (req, res) => {
-  const rentalID = req.params.rentalID;
+    const rentalID = req.params.rentalID;
 
-  try {
-    const rental = await Rental.findById(rentalID);
+    try {
+        const rental = await Rental.findById(rentalID);
 
-    if (!rental) {
-      return res.status(404).json({ error: "Rental not found" });
+        if (!rental) {
+            return res.status(404).json({
+                error: "Rental not found"
+            });
+        }
+
+        await rental.deleteOne();
+
+        res.json({
+            message: "Book return successful"
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Failed to process book return"
+        });
     }
-
-    await rental.deleteOne();
-
-    res.json({ message: "Book return successful" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to process book return" });
-  }
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
--- view/outs.js
+++ view/outs.js
@@ -11,48 +11,52 @@ document.addEventListener("DOMContentLoaded", async () => {
     console.error("Error fetching and displaying rental details:", error);
   }
 
-  document.getElementById("filterOverdueBtn").addEventListener("click", async () => {
-    document.getElementById("table-container").innerHTML = "";
+  document
+    .getElementById("filterOverdueBtn")
+    .addEventListener("click", async () => {
+      document.getElementById("table-container").innerHTML = "";
 
-    try {
-      const overdueRentalData = await fetchOverdueRentalDetails();
-      
-      if (overdueRentalData.length > 0) {
-        const overdueTable = createRentalTable(overdueRentalData);
-        displayRentalTable(overdueTable);
-      } else {
-        displayNoRentalDataMessage();
+      try {
+        const overdueRentalData = await fetchOverdueRentalDetails();
+        if (overdueRentalData.length > 0) {
+          const overdueTable = createRentalTable(overdueRentalData);
+          displayRentalTable(overdueTable);
+        } else {
+          displayNoRentalDataMessage();
+        }
+      } catch (error) {
+        console.error(
+          "Error fetching and displaying overdue rental details:",
+          error
+        );
       }
-    } catch (error) {
-      console.error("Error fetching and displaying overdue rental details:", error);
-    }
-  });
+    });
 
-  document.getElementById("allRentalsBtn").addEventListener("click", async () => {
-    document.getElementById("table-container").innerHTML = "";
+  document
+    .getElementById("allRentalsBtn")
+    .addEventListener("click", async () => {
+      document.getElementById("table-container").innerHTML = "";
 
-    try {
-      const overdueRentalData = await fetchRentalDetails();
-      
-      if (overdueRentalData.length > 0) {
-        const overdueTable = createRentalTable(overdueRentalData);
-        displayRentalTable(overdueTable);
-      } else {
-        displayNoRentalDataMessage();
+      try {
+        const allRentalData = await fetchRentalDetails();
+
+        if (allRentalData.length > 0) {
+          const allDataTable = createRentalTable(allRentalData);
+          displayRentalTable(allDataTable);
+        } else {
+          displayNoRentalDataMessage();
+        }
+      } catch (error) {
+        console.error("Error fetching and displaying  rental details:", error);
       }
-    } catch (error) {
-      console.error("Error fetching and displaying overdue rental details:", error);
-    }
-  });
+    });
 });
-
-
 
 function displayNoRentalDataMessage() {
   const noDataMessage = document.createElement("p");
   noDataMessage.textContent = "No rental data available.";
   noDataMessage.classList.add("no-data-message");
-  
+
   const rentalTableContainer = document.getElementById("table-container");
   rentalTableContainer.appendChild(noDataMessage);
 }
@@ -68,10 +72,12 @@ async function fetchRentalDetails() {
 
 async function fetchOverdueRentalDetails() {
   const response = await fetch("http://localhost:3000/overdue-rentals");
+
   if (!response.ok) {
     throw new Error("Failed to fetch overdue rental details");
   }
   const { data } = await response.json();
+  console.log(data);
   return data;
 }
 
@@ -120,9 +126,10 @@ function createTableRow(rentalDetail) {
 
   const cells = ["studentName", "schoolID", "contact", "dueDate"];
   cells.forEach((cell) => {
-    const cellData = (cell === "dueDate") ?
-      new Date(rentalDetail[cell]).toLocaleDateString() :
-      rentalDetail[cell];
+    const cellData =
+      cell === "dueDate"
+        ? new Date(rentalDetail[cell]).toLocaleDateString()
+        : rentalDetail[cell];
 
     const tableCell = document.createElement("td");
     tableCell.textContent = cellData;
@@ -136,36 +143,36 @@ function createTableRow(rentalDetail) {
 }
 
 const calculateFine = (date) => {
-
-  console.log(date)
   const currentDate = new Date();
   const dueDate = new Date(date);
-  console.log(currentDate,dueDate)
 
   if (currentDate > dueDate) {
     const timeDiff = currentDate.getTime() - dueDate.getTime();
-    const daysOverdue = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+    const daysOverdue = Math.ceil(timeDiff / (1000 * 3600 * 24));
     const fine = daysOverdue * 50;
-    return `$${fine.toString()}`; 
+    return `$${fine.toString()}`;
   } else {
     return "-";
   }
 };
-
 
 function createRemovalCell(rentalDetail) {
   const removalCell = document.createElement("td");
   const removalBtn = document.createElement("button");
   removalBtn.classList.add("sm-blue-btn");
   removalCell.appendChild(removalBtn);
- 
-  const calculatedFine = calculateFine(rentalDetail.dueDate)
 
-  removalBtn.textContent = (calculatedFine == "-" ? 'Return' : `Return & pay ${calculateFine(rentalDetail.dueDate)} `)
+  const calculatedFine = calculateFine(rentalDetail.dueDate);
 
-    
-  removalBtn.classList.add(calculatedFine == "-" ? "sm-blue-btn" : "sm-red-btn" );
-  
+  removalBtn.textContent =
+    calculatedFine == "-"
+      ? "Return"
+      : `Return & pay ${calculateFine(rentalDetail.dueDate)} `;
+
+  removalBtn.classList.add(
+    calculatedFine == "-" ? "sm-blue-btn" : "sm-red-btn"
+  );
+
   removalBtn.addEventListener("click", async () => {
     try {
       const rentalID = rentalDetail._id;
@@ -175,13 +182,11 @@ function createRemovalCell(rentalDetail) {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ bookToRemove: "Some Book Title" }),
       });
 
       if (response.ok) {
         removalCell.parentElement.remove();
-        console.log("Book removed successfully");
-        window.location.reload()
+        window.location.reload();
       } else {
         console.error("Failed to remove book");
       }
